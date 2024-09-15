7f8263f3100e58236c163b05ed907972e34bb20b
Али Мараз <alierik0404@gmail.com>
2024-09-15 13:39:29 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ import json
 from google.oauth2.service_account import Credentials
 
 creds_data = os.environ.get('GOOGLE_CREDENTIALS')
-creds_dict = json.loads(creds_data)
-creds = Credentials.from_service_account_info(creds_dict, scopes=[
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-])
-
+if creds_data is None:
+    print("GOOGLE_CREDENTIALS переменная окружения не найдена.")
+else:
+    creds_dict = json.loads(creds_data)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=[
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ])
